93054f5f88c57de80f41704370f3392c88d75d0d
Gia Tien Truong <truongtien577@gmail.com>
2023-12-21 21:20:47 +0700

    Filter contract by status API

--- ads_management/src/main/java/com/group6/ads/repositories/database/contracts/ContractRepository.java
+++ ads_management/src/main/java/com/group6/ads/repositories/database/contracts/ContractRepository.java
@@ -8,6 +8,13 @@ public interface ContractRepository extends JpaRepository<Contract, Integer> {
     @Query("""
             SELECT c
             FROM Contract c
+            WHERE c.status = %:status% AND (c.companyName LIKE %:search% OR c.companyEmail LIKE %:search% OR c.companyPhone LIKE %:search%)
+            """)
+    Page<Contract> findAll(String search, Integer status, Pageable pageable);
+
+    @Query("""
+            SELECT c
+            FROM Contract c
             WHERE (c.companyName LIKE %:search% OR c.companyEmail LIKE %:search% OR c.companyPhone LIKE %:search%)
             """)
     Page<Contract> findAll(String search, Pageable pageable);
